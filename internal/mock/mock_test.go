ddc5c90e393abc6d08011ee5eef0e94500a4b013
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-15 11:35:30 +0300

    Add test for jsonizing.

--- internal/mock/mock_test.go
+++ internal/mock/mock_test.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -14,4 +15,13 @@ func TestGetStaticOutput(t *testing.T) {
 	if !cmp.Equal(result, correct) || err != nil {
 		t.Errorf("Got: %v, want: %v.", result, correct)
 	}
+}
+
+func TestJsonizeStaticOutput(t *testing.T) {
+	correct := ""
+	result, err := JsonizeStaticOutput()
+	if strings.Compare(result, correct) != 1 || err != nil {
+		t.Errorf("Got: %s, want: %s.", result, correct)
+	}
+
 }
