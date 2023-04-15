441491116db49fe4d8da26de752df41cc62932d7
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-15 11:57:29 +0300

    Add jsonizing function.

--- internal/mock/mock.go
+++ internal/mock/mock.go
@@ -1,11 +1,25 @@
 package mock
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type MockTemperatureStats struct {
 	Temperature int     `json:"temperature"`
 	Humidity    float32 `json:"humidity"`
 	Pressure    int     `json:"pressure"`
 }
 
-func GetStaticOutput() (MockTemperatureStats, error) {
-	return MockTemperatureStats{15, 0.6, 1000}, nil
+func GetStaticOutput() MockTemperatureStats {
+	return MockTemperatureStats{15, 0.6, 1000}
+}
+
+func JsonizeStaticOutput() ([]byte, error) {
+	jsonOutput, err := json.Marshal(GetStaticOutput())
+	if err != nil {
+		fmt.Printf("Error during jsonizing: %s", err)
+		return nil, err
+	}
+	return jsonOutput, nil
 }
