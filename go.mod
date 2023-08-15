71491bde00764e0f78f4f32c69eadbcdfeba784e
Slav Ivanov <ivanov.slk@abv.bg>
2023-08-15 20:00:29 +0300

    The acceptance test times out correctly.

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.20
 require github.com/testcontainers/testcontainers-go v0.22.0
 
 require (
+	github.com/nats-io/nkeys v0.4.4 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+)
+
+require (
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
@@ -18,12 +24,13 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/moby/patternmatcher v0.5.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/nats-io/nats.go v1.28.0
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc4 // indirect
 	github.com/opencontainers/runc v1.1.5 // indirect
--- internal/adapters/nats/driver.go
+++ internal/adapters/nats/driver.go
@@ -1,9 +1,12 @@
 package nats
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
 
 	nats_server "github.com/ivanov-slk/tma-data-generator/test/specifications/nats"
+	"github.com/nats-io/nats.go"
 	"github.com/testcontainers/testcontainers-go"
 )
 
@@ -13,6 +16,40 @@ type DataGenerator struct {
 }
 
 func (dg *DataGenerator) GenerateData() ([]byte, error) {
-	fmt.Printf("%v", dg.NatsServer)
-	return []byte{}, nil
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	nc, err := nats.Connect(dg.NatsServer.URI)
+	if err != nil {
+		log.Fatalf("failed to connect to nats: %s", err)
+	}
+	defer nc.Close()
+
+	// perform assertions
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatalf("failed to create jetstream context: %s", err)
+	}
+
+	// add stream to nats
+	if _, err = js.AddStream(&nats.StreamConfig{
+		Name:     "test-data-generator",
+		Subjects: []string{"data-generator-subject"},
+	}); err != nil {
+		log.Fatalf("failed to add stream: %s", err)
+	}
+
+	// add subscriber to nats
+	sub, err := js.SubscribeSync("data-generator-subject", nats.Durable("worker"))
+	if err != nil {
+		log.Fatalf("failed to subscribe to hello: %s", err)
+	}
+
+	// wait for the message to be received
+	msg, err := sub.NextMsgWithContext(ctx)
+	if err != nil {
+		log.Fatalf("failed to get message: %s", err)
+	}
+
+	return msg.Data, nil
 }
