07619b8568f2617db148565de14a37f2e65adf31
Slav Ivanov <ivanov.slk@abv.bg>
2023-09-03 21:01:10 +0300

    chore: Add log mode to error messages.

--- cmd/main.go
+++ cmd/main.go
@@ -13,13 +13,13 @@ func main() {
 	log.Println("INFO: Initializing the data generator service...")
 	natsURI, found := os.LookupEnv("NATS_SERVER_URI")
 	if !found {
-		log.Fatal("NATS server URI not set.")
+		log.Fatal("ERROR: NATS server URI not set.")
 	}
 
 	log.Printf("INFO: Will connect to NATS server at %s\n", natsURI)
 	nc, err := nats.Connect(natsURI)
 	if err != nil {
-		log.Fatalf("failed to connect to nats server: %s", err)
+		log.Fatalf("ERROR: failed to connect to nats server: %s", err)
 	}
 	defer nc.Close()
 
